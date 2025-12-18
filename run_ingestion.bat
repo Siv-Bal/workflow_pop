a07083725e38aee74a8ebd103df96a4bcdc2b9c5
Siv-Bal <sivabalan.cse06@gmail.com>
2025-12-18 18:12:23 +0530

    Add files via upload

---
+++ run_ingestion.bat
@@ -0,0 +1,7 @@
+@echo off
+cd C:\Users\Kirus\Documents\workflow_pop
+
+python -m fetcher.youtube_fetcher
+python -m fetcher.forum_fetcher
+
+echo Ingestion completed at %DATE% %TIME% >> ingestion.log
